5a36d434c128d945392251d1c054e001a3cf0189
GTrunSec <gtrunsec@hardenedlinux.org>
2022-09-02 19:04:23 -0700

    chore: org code

--- nix/vast/nixago/vast-integation.nix
+++ nix/vast/nixago/vast-integation.nix
@@ -4,40 +4,11 @@
 } @ args: let
   inherit (inputs) nixpkgs;
   zeek = import ./zeek.nix args;
+  API = import ./API.nix args;
 in {
   tests =
     zeek
+    // API
     // {
-      /*
-      Google search search API
-      */
-      google-search-api = {
-        tags = ["schema" "json"];
-        steps = [
-          {
-            command = "-N import -s ${cell.schemas.google-search-api} -t google.search.api json";
-            transformation = "jq -ec '.[]' ./data/API/google-search-api.json";
-          }
-          {
-            command = ''-N export json '#type == "google.search.api"' '';
-          }
-        ];
-      };
-
-      /*
-      Google phishing search API
-      */
-      google-phishing-api = {
-        tags = ["schema" "json"];
-        steps = [
-          {
-            command = "-N import -s ${cell.schemas.google-phishing-api} -t google.phishing.api json";
-            transformation = "jq -ec '.[]' ./data/API/google-phishing-api.json";
-          }
-          {
-            command = ''-N export json '#type == "google.phishing.api"' '';
-          }
-        ];
-      };
     };
 }

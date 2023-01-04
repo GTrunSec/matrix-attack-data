bcbe6cf1aa3b5671ed696981caefedb4d0c3e8bb
guangtao <gtrunsec@hardenedlinux.org>
2023-01-03 23:00:04 -0800

    id fix

--- nix/mitre-attack/config.nix
+++ nix/mitre-attack/config.nix
@@ -8,5 +8,5 @@
   l = inputs.nixpkgs.lib // builtins;
   nist800_53_r4-controls' = l.fromJSON (l.readFile (attack-control-framework-mappings + "/frameworks/attack_10_1/nist800_53_r4/stix/nist800-53-r4-controls.json"));
 in {
-  nist800_53_r4-controls = l.foldl' l.recursiveUpdate {} (map (v: { "${v.created}" = v;}) nist800_53_r4-controls'.objects);
+  nist800_53_r4-controls = l.foldl' l.recursiveUpdate {} (map (v: { "${v.id}" = v;}) nist800_53_r4-controls'.objects);
 }
